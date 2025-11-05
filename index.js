e093a875554887a40fa436d0170b11046266cd49
Imtiazul Hoque <imtiazulhoque8936@gmail.com>
2025-11-05 19:35:51 +0600

    project initialized

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dbz4f6f.mongodb.net/?appName=Cluster0`;
+
+// localDB
+// const uri = `${process.env.DB_LOCAL}`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("smart_db");
+    const productsCollection = db.collection("products");
+
+    app.post("/new", async (req, res) => {
+      const newUser = req.body;
+      const result = await productsCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    app.get("/", async (req, res) => {
+      const result = await productsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/:id", async (req, res) => {
+      const { id } = req.params;
+      // console.log(id);
+      const result = await productsCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Smart server is running on port: ${port}`);
+});
